9bf5743fc84e1aca14e77bf324ca830795050e87
thKimm <90517083+thKimm@users.noreply.github.com>
2024-02-21 16:50:09 +0900

    [Silver I] Title: IOIOI, Time: 436 ms, Memory: 74440 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,6 +2,8 @@
 Point
 1. 이진 탐색
 
+공부
+1. 시간 초과를 피하기 위해선 KMP 알고리즘
 '''
 import sys
 input = sys.stdin.readline
@@ -11,6 +13,41 @@ M = int(input().strip())
 S = 'IO'*N + 'I'
 T = input().strip()
 result = 0 
-for i in range(len(T)-2*N):
-    result += int(T[i:i+len(S)] == S)
-print(result)
+flag = False
+# for i in range(M-2*N):
+#     for j in range(2*N+1):
+#         if S[j] != T[i+j]:
+#             flag = True
+#             break
+#     result += int(not flag)
+#     if not flag:
+#         i += 2
+#     flag = False
+def kmp(all_string, pattern):
+    #  kmp_table
+    table = [0 for _ in range(len(pattern))]
+    i = 0
+    for j in range(1, len(pattern)):
+        while i > 0 and pattern[i] != pattern[j]:
+            i = table[i-1]
+
+        if pattern[i] == pattern[j]:
+            i += 1
+            table[j] = i
+
+    #  kmp
+    result = []
+    i = 0
+    for j in range(len(all_string)):
+        while i > 0 and pattern[i] != all_string[j]:
+            i = table[i-1]
+
+        if pattern[i] == all_string[j]:
+            i += 1
+            if i == len(pattern):
+                result.append(j - i + 1)
+                i = table[i-1]
+
+    return result
+    
+print(len(kmp(T,S)))
